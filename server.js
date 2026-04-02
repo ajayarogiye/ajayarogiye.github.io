7db8a39ff22228077faa5a0eb85e7ff10db96f94
ajayarogiye <tradingajaya@gmail.com>
2026-04-02 13:45:25 +0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.static("."));
+
+// Fallback for SPA
+app.use((req, res) => {
+  res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.listen(5000, "0.0.0.0", () => console.log("Server running on port 5000"));
